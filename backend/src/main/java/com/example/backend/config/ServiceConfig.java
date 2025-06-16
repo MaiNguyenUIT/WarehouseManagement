8dad5dd197c24ad76025979e8331c14bef90bc98
lamden000 <lamden150@gmail.com>
2025-06-16 20:57:42 +0700

    add usage for concrete decorators

--- backend/src/main/java/com/example/backend/config/ServiceConfig.java
+++ backend/src/main/java/com/example/backend/config/ServiceConfig.java
@@ -1,7 +1,10 @@
 package com.example.backend.config;
 
+import com.example.backend.pattern.Decorator.CachingImportShipmentServiceDecorator;
 import com.example.backend.pattern.Decorator.LoggingAndValidatingImportShipmentServiceDecorator;
+import com.example.backend.pattern.Decorator.SecurityImportShipmentServiceDecorator;
 import com.example.backend.service.ImportShipmentService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -9,14 +12,16 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class ServiceConfig {
 
-    @Bean
+    @Bean("importShipmentServiceImpl")
     public ImportShipmentService importShipmentServiceImpl() {
         return new com.example.backend.serviceImpl.ImportShipmentService();
     }
 
     @Bean
     @Primary
-    public ImportShipmentService importShipmentService(ImportShipmentService importShipmentServiceImpl) {
-        return new LoggingAndValidatingImportShipmentServiceDecorator(importShipmentServiceImpl);
+    public ImportShipmentService importShipmentService(@Qualifier("importShipmentServiceImpl") ImportShipmentService importShipmentServiceImpl) {
+        ImportShipmentService serviceWithCaching = new CachingImportShipmentServiceDecorator(importShipmentServiceImpl);
+        ImportShipmentService serviceWithLoggingAndValidation = new LoggingAndValidatingImportShipmentServiceDecorator(serviceWithCaching);
+        return new SecurityImportShipmentServiceDecorator(serviceWithLoggingAndValidation);
     }
 }
